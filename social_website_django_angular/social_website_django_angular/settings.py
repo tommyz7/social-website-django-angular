ae910f7950235316331f13bc3a006bc676dc7d6c
Tomasz Zacharczuk <tomasz.zacharczuk@gmail.com>
2016-11-06 19:28:57 +0100

    Update settings for templates and static files

--- social_website_django_angular/social_website_django_angular/settings.py
+++ social_website_django_angular/social_website_django_angular/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'social_website_django_angular.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +118,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'staticfiles'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
+
