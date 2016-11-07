671e0456127645ef99332ea75cebac2dae227e0a
Tomasz Zacharczuk <tomasz.zacharczuk@gmail.com>
2016-11-07 12:52:51 +0100

    Add custom permission for AccountViewSet and serializer for Account model

---
+++ social_website_django_angular/authentication/serializers.py
@@ -0,0 +1,37 @@
+from django.contrib.auth import update_session_auth_hash
+from rest_framework import serializers
+
+from authentication.models import Account
+
+
+class AccountSerializer(serializers.ModelSerializer):
+	password = serializers.CharField(write_only=True, required=False)
+	confirm_password = serializers.CharField(write_only=True, required=False)
+
+	class Meta:
+		model = Account
+		fields = ('id', 'email', 'username', 'first_name', 'last_name',
+				  'tagline', 'created', 'updated', 'password', 
+				  'confirm_password')
+		read_only_fields = ('created', 'updated')
+
+	def create(self, validated_data):
+		return Account.objects.create(**validated_data)
+
+	def update(self, instance, validated_data):
+		instance.email = validated_data.get('email', instance.email)
+		instance.username = validated_data.get('username', instance.username)
+		instance.first_name = validated_data.get('first_name', instance.first_name)
+		instance.last_name = validated_data.get('last_name', instance.last_name)
+		instance.tagline = validated_data.get('tagline', instance.tagline)
+
+		instance.save()
+
+		password = validated_data.get('password')
+		if password and confirm_password and password == confirm_password:
+			instance.set_password(password)
+			instance.save()
+
+			update_session_auth_hash(self.context.get('request'), instance)
+
+		return instance
