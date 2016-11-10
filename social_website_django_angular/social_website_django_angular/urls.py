48a6a934e165f8e41b05a83a7f09bae9ab4f13d6
Tomasz Zacharczuk <tomasz.zacharczuk@gmail.com>
2016-11-10 13:00:45 +0100

    Fix typo

--- social_website_django_angular/social_website_django_angular/urls.py
+++ social_website_django_angular/social_website_django_angular/urls.py
@@ -26,6 +26,6 @@ router.register(r'accounts', AccountViewSet)
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^api/v1/', include(router.urls))
+    url(r'^api/v1/', include(router.urls)),
     url('^.*$', IndexView.as_view(), name='index')
 ]
