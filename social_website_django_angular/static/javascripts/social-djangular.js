0b559d349244d35355d0f5a7dd50fb335ef4df8a
Tomasz Zacharczuk <tomasz.zacharczuk@gmail.com>
2016-11-10 13:19:36 +0100

    Add angular CSRF support

--- social_website_django_angular/static/javascripts/social-djangular.js
+++ social_website_django_angular/static/javascripts/social-djangular.js
@@ -11,3 +11,13 @@ angular
 angular
 	.module('social_djangular.routes', ['ngRoute']);
 
+angular
+	.module('social_djangular')
+	.run(csrf_token);
+
+csrf_token.$inject = ['$http'];
+
+function csrf_token($http){
+	$http.defaults.xsrfHeaderName = 'X-CSRFToken';
+	$http.defaults.xsrfCookieName = 'csrftoken';
+}
