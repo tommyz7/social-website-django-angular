0e42c6ba34938d967af1200f130c49117810733f
Tomasz Zacharczuk <tomasz.zacharczuk@gmail.com>
2016-12-01 17:56:13 +0100

    Add login route to angular, login template, add login js file to django javascript

--- social_website_django_angular/static/javascripts/social-djangular.routes.js
+++ social_website_django_angular/static/javascripts/social-djangular.routes.js
@@ -12,6 +12,10 @@
 			controller: 'RegisterController',
 			controllerAs: 'regCtl',
 			templateUrl: 'static/templates/authentication/register.html'
+		}).when('/login', {
+		    controller: 'LoginController',
+		    controllerAs: 'loginCtl',
+		    templateUrl: 'static/templates/authentication/login.html'
 		}).otherwise('/');
 	}
 })();
