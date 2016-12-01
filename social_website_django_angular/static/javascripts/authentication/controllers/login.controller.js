8fe3b7819de1859f956013bcac62127c1682d60a
Tomasz Zacharczuk <tomasz.zacharczuk@gmail.com>
2016-12-01 17:54:01 +0100

    Add login controller and login service with helper functions

---
+++ social_website_django_angular/static/javascripts/authentication/controllers/login.controller.js
@@ -0,0 +1,23 @@
+(function(){
+    'use strict';
+
+    angular
+        .module('social_djangular.authentication.controllers')
+        .controller('LoginController', LoginController);
+
+    LoginController.$inject = ['$location', '$scope', 'Authentication'];
+    function LoginController($location, $scope, Authentication){
+        var loginCtl = this;
+        loginCtl.login = login;
+        activate();
+
+        function activate(){
+            if (Authentication.isAuthenticated()) {
+                $location.url('/');
+            }
+        }
+        function login(){
+            Authentication.login(loginCtl.email, loginCtl.password);
+        }
+    }
+})();
--- social_website_django_angular/static/javascripts/authentication/services/authentication.service.js
+++ social_website_django_angular/static/javascripts/authentication/services/authentication.service.js
@@ -9,10 +9,52 @@
 
 	function Authentication($cookies, $http){
 		var Authentication = {
+		    getAuthenticatedAccount: getAuthenticatedAccount,
+		    setAuthenticatedAccount: setAuthenticatedAccount,
+		    isAuthenticated: isAuthenticated,
+		    unauthenticate: unauthenticate,
+		    login: login,
 			register: register
 		};
 
 		return Authentication;
+
+		function getAuthenticatedAccount(){
+		    if (!$cookies.authenticatedAccount){
+		        return;
+		    }
+
+		    return JSON.parse($cookies.authenticatedAccount)
+		}
+
+		function isAuthenticated(){
+	        return !!$cookies.authenticatedAccount;
+	    }
+
+	    function setAuthenticatedAccount(account){
+	        $cookies.authenticatedAccount = JSON.stringify(account);
+	    }
+
+	    function unauthenticate(){
+	        delete $cookies.authenticatedAccount;
+	    }
+
+        function login(email, password){
+            $http.post('/api/v1/auth/login/', {
+                email: email,
+                password: password
+            }).then(loginSuccessFn, loginErrorFn);
+
+            function loginSuccessFn(data, status, headers, config){
+                Authentication.setAuthenticatedAccount(data.data);
+
+                window.location = '/';
+            }
+
+            function loginErrorFn(data, status, headers, config){
+                console.log('Login error.');
+            }
+        }
 
 		function register(email, password, username){
 			$http.post('/api/v1/accounts', {
