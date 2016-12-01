ded150eeb3cde326b55956cc216f2fbd656e437c
Tomasz Zacharczuk <tomasz.zacharczuk@gmail.com>
2016-12-01 17:46:28 +0100

    Create API login view
    Creates login endpoint for angular login controller.
    Commit includes indentation fix.

--- social_website_django_angular/authentication/views.py
+++ social_website_django_angular/authentication/views.py
@@ -1,7 +1,7 @@
 import json
 from django.contrib.auth import authenticate, login
 from rest_framework.response import Response
-from rest_framework import permissions, status, viewsets
+from rest_framework import permissions, status, viewsets, views
 
 from .models import Account
 from .serializers import AccountSerializer
@@ -32,7 +32,33 @@ class AccountViewSet(viewsets.ModelViewSet):
                 serializer.validated_data, status=status.HTTP_201_CREATED
             )
 
-		return Response({
-			'status': 'Bad request',
-			'message': 'Account could not be created.'
-			}, status=status.HTTP_400_BAD_REQUEST)
+        return Response({
+            'status': 'Bad request',
+            'message': 'Account could not be created.'
+        }, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LoginView(views.APIView):
+    def post(self, request, format=None):
+        email = request.data.get('email', None)
+        password = request.data.get('password', None)
+
+        account = authenticate(email=email, password=password)
+
+        if account is not None:
+            if account.is_active:
+                login(request, account)
+
+                serialized = AccountSerializer(account)
+
+                return Response(serialized.data)
+            else:
+                return Response({
+                    'status': 'Unauthorized',
+                    'message': 'This account has been blocked'
+                }, status=status.HTTP_401_UNAUTHORIZED)
+        else:
+            return Response({
+                'status': 'Unauthorized',
+                'message': 'Username/password combination invalid'
+            }, status=status.HTTP_401_UNAUTHORIZED)
